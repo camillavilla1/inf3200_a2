2d6c9150b7b932f61ebd62eac265d2b60aaac7c2
sigurdtho <sigurd14@gmail.com>
2016-10-31 15:53:02 +0100

    basics

--- inf-3200-assignment2-handout/node.py
+++ inf-3200-assignment2-handout/node.py
@@ -1,16 +1,61 @@
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from SocketServer import ThreadingMixIn
 import threading
+import re
+import argparse
+import httplib
+import signal
+import socket
+
+nodeList = []
+neighbors = []
+
+
+class Node():
+    pass
+
 
 class Handler(BaseHTTPRequestHandler):
+    def extract_key_from_path(self, path):
+        return re.sub(r'/?(\w+)', r'\1', path)
 
     def do_GET(self):
+        key = self.extract_key_from_path(self.path)
+        
+        if (key == "/neighbours"):
+            #node.neighbours()
+            #message = str(node.ip + node.port)
+            pass
+
+
+
         self.send_response(200)
         self.end_headers()
-        message =  threading.currentThread().getName()
+        #message =  threading.currentThread().getName()
+        message = "dildo"
         self.wfile.write(message)
         self.wfile.write('\n')
         return
+
+    def do_POST(self):
+        key = self.extract_key_from_path(self.path)
+        self.send_response(200)
+        self.end_headers()
+        if (key == "/addNode"):
+            #node.Add()
+            pass
+        if (key == "/shutdown"):
+            #node.shutdown()
+            pass
+        
+
+        message = ("/addNode\n")
+
+
+        self.wfile.write(message)
+
+
+
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     """Handle requests in a separate thread."""
