c776dbd796245a746423bc02720ae6854e01ccce
camilla <dc-milla@hotmail.com>
2016-10-31 08:01:40 +0100

    first commit

---
+++ inf-3200-assignment2-handout/README.md
@@ -0,0 +1,37 @@
+Test Utilities for INF-3200, Fall 2016, Assignment 2
+=====================================================
+
+This repository contains of two quick-and-dirty test clients for your project
+networks.
+
+- `test-client.py` -- Uses the network's API to connect to a single node and
+  issue commands to fire up and shut down more nodes.
+- `network-grapher.py` -- Uses the API to crawl the network and build a list of
+  how the nodes are connected.
+
+Each takes one argument, the node to connect to.
+
+    ./test-client.py localhost:8000
+    ./network-grapher.py localhost:8000
+
+Like with the previous project, we will be using more thorough versions of these
+utilities to test your network in action. We strongly suggest that you add your
+own enhancements to these clients to perform more thorough tests. Think about
+different scenarios and interesting corner cases:
+
+- What happens as the network gets much larger?
+- What happens as churn increases (faster joins and leaves)?
+- What happens when multiple startup and shutdown requests are executed in
+  parallel?
+
+Hints
+--------------------------------------------------
+
+- Try running the `network-grapher` with the Unix `watch` command to re-run and
+  print the output every few seconds.
+
+        watch ./network-grapher.py localhost:8000
+
+- With a little massaging, you might be able to get the `network-grapher` to
+  print valid descriptions for [Graphviz]( http://www.graphviz.org/), or another
+  visualization library, to get an actual graphical map of your network.
---
+++ inf-3200-assignment2-handout/network-grapher.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+import argparse
+import collections
+import httplib
+import socket
+import unittest
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser(prog="network-grapher",
+            description="Quick and dirty utility to find which nodes are connected to what other nodes")
+
+    parser.add_argument("node",
+            help="starter node to connect to first")
+
+    return parser.parse_args()
+
+def fetch_neighbors(hostport):
+    conn = httplib.HTTPConnection(hostport)
+    conn.request("GET", "/neighbours")
+    r1 = conn.getresponse()
+    data1 = r1.read()
+    conn.close()
+    if r1.status != 200:
+        raise RuntimeError("Error {0} {1} from {2}".format(r1.status, r1.reason, hostport))
+    else:
+        return parse_neighbors(data1)
+
+def parse_neighbors(neighborstring):
+    stripped = neighborstring.strip()
+    if stripped == '':
+        return []
+    else:
+        return stripped.split("\n")
+
+class ParseTests(unittest.TestCase):
+    def test_zero_neighbors(self):
+        self.assertEqual(
+                parse_neighbors(""),
+                [])
+
+    def test_one_neighbor(self):
+        self.assertEqual(
+                parse_neighbors("localhost:8000"),
+                ["localhost:8000"])
+
+    def test_two_neighbors(self):
+        self.assertEqual(
+                parse_neighbors("localhost:8000\nlocalhost:8001"),
+                ["localhost:8000", "localhost:8001"])
+
+    def test_three_neighbors(self):
+        self.assertEqual(
+                parse_neighbors("localhost:8000\nlocalhost:8001\nlocalhost:8002"),
+                ["localhost:8000", "localhost:8001", "localhost:8002"])
+
+def probe_network(known_servers):
+    to_check = collections.deque(known_servers)
+    checked = []
+    graph = {}
+    while len(to_check):
+        server = to_check.popleft()
+        if server not in graph:
+            try:
+                neighbors = fetch_neighbors(server)
+                graph[server] = neighbors
+                for n in neighbors:
+                    if n not in graph:
+                        to_check.append(n)
+            except socket.error as e:
+                graph[server] = e
+    return graph
+
+def print_graph(graph):
+    for server,neighbors in graph.iteritems():
+        if isinstance(neighbors, socket.error):
+            print "{0} -> {1}".format(server, repr(neighbors))
+        else:
+            print "{0} -> {{ {1} }}".format(server, ",".join(neighbors))
+
+if __name__ == "__main__":
+
+    args = parse_args()
+    graph = probe_network([args.node])
+    print_graph(graph)
